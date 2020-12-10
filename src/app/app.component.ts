c05679649f37d1471d708c620e6b461666707560
Denis <dineresc@gmail.com>
2020-12-10 01:57:41 -0300

    impl logic on component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'demo-directive';
+  
+  counter1:number = 0;
+  counter2:number = 0;
+  counter3:number = 0;
+  
+  incMouseoverCounter(cardId) {
+    switch(cardId) {
+      case "1": this.counter1++;break;
+      case "2": this.counter2++; break;
+      case "3": this.counter3++; break;
+    }
+  }
 }
